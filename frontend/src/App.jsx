a1b2a0003c6f0ce4df993cd431c021d21e972be7
Aranga Sakthivel R <157717490+arangasakthivel@users.noreply.github.com>
2026-01-03 19:58:10 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import AdminDashboard from "./pages/admin/AdminDashboard";
 import StaffDashboard from "./pages/staff/StaffDashboard";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
-import CursorGlow from "../src/components/cursorGlow/CursorGlow";
+import CursorGlow from "../src/components/cursorGlow";
 
 function App() {
   return (
