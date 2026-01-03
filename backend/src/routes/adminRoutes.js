47b4898559b0a8a9d0b38f4b5e0fe5ab3a2b7f03
Aranga Sakthivel R <157717490+arangasakthivel@users.noreply.github.com>
2026-01-03 17:50:11 +0530

    Update adminRoutes.js

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { protect, isAdmin } = require("../middleware/authmiddleware");
+const { protect, isAdmin } = require("../middleware/authMiddleware");
 const { getAnalytics } = require("../controllers/adminController");
 
 router.get("/analytics", protect, isAdmin, getAnalytics);
